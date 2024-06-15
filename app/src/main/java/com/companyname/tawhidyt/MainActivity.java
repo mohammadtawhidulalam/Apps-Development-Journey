e8f3e868fc07cd2b182d0271bf46341fa90b96bb
mohammadtawhidulalam <tohidislam192@gmail.com>
2024-06-15 08:31:37 +0600

    Digital Tasbih

--- app/src/main/java/com/companyname/tawhidyt/MainActivity.java
+++ app/src/main/java/com/companyname/tawhidyt/MainActivity.java
@@ -1,6 +1,9 @@
 package com.companyname.tawhidyt;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button bAdd, bSub, bReset;
+    int count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+tvDisplay = findViewById(R.id.tvDiplay);
+bAdd = findViewById(R.id.bAdd);
+bSub = findViewById(R.id.bSub);
+bReset = findViewById(R.id.bReset);
+
+//        .................................................................
+
+        bAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                tvDisplay.setText(""+count);
+            }
+        });
+
+        bSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count--;
+                tvDisplay.setText(""+count);
+
+            }
+        });
+
+        bReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count =0;
+                tvDisplay.setText(""+count);
+
+            }
+        });
+
+
     }
 }
